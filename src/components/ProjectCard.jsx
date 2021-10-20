370847d10f974bddd1d4169cc8f38fbaf25b4bd1
Ahmed Roble <airoble2019@gmail.com>
2021-10-20 00:08:50 -0400

    Add feature user likes

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,74 @@
-import { AiOutlineFire } from "react-icons/ai"
+import { useMutation, gql, useQuery } from "@apollo/client"
+import { AiOutlineFire, AiFillFire } from "react-icons/ai"
 import { Card, Button, Col } from "react-bootstrap"
 import "./../App.css"
 import classes from "./ProjectCard.module.css"
 import { useHistory } from "react-router"
+import { useContext } from "react"
+import { UserContext } from "../store/UserContext"
+
+const CREATE_LIKE = gql`
+  mutation AddOneLike($input: createLikeInput!) {
+    createLike(input: $input) {
+      like {
+        id
+      }
+    }
+  }
+`
+
+const COUNT_LIKES = gql`
+  query likesPerProject($projectID: ID!) {
+    likes(where: { project: { id: $projectID } }) {
+      id
+      user {
+        id
+        firstName
+        lastName
+      }
+    }
+  }
+`
+
 const ProjectCard = ({ project }) => {
   const history = useHistory()
+  const { user } = useContext(UserContext)
+
+  // create like for project and refetch the likes count
+  const [CreateLikeMutation, { error: likeError }] = useMutation(CREATE_LIKE, {
+    refetchQueries: [
+      { query: COUNT_LIKES, variables: { projectID: project.id } },
+    ],
+  })
+  // Initial likes count
+  const { error, data, loading } = useQuery(COUNT_LIKES, {
+    variables: { projectID: project.id },
+  })
+
+  // function handles like clicks
+  const Addlike = () => {
+    if (!user) history.push("/login")
+    else {
+      CreateLikeMutation({
+        variables: {
+          input: {
+            data: {
+              project: project.id,
+              user: user.userId,
+            },
+          },
+        },
+      })
+    }
+  }
+  console.log(data)
+
+  const checkIfProjectLiked = function (likes, userId) {
+    return userId ? likes.find((like) => like.user.id === userId) : false
+  }
+
+  if (loading) return <p>Loading...</p>
+  if (error || likeError) return <p>Error</p>
   return (
     <Col className="gy-3">
       <Card className={classes.box}>
@@ -16,8 +80,12 @@ const ProjectCard = ({ project }) => {
             alt={`${project.developer.firstName} ${project.developer.lastName}`}
           ></img>
           <div className="d-flex flex-column align-items-center">
-            <AiOutlineFire size="2.5rem" />
-            <span>{project.likesCount}</span>
+            {checkIfProjectLiked(data.likes, user.userId) ? (
+              <AiFillFire color="red" size="2.5rem" onClick={Addlike} />
+            ) : (
+              <AiOutlineFire size="2.5rem" onClick={Addlike} />
+            )}
+            <span>{data.likes.length}</span>
           </div>
         </div>
         <Card.Body>
