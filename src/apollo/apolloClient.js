521a8d5452c7bed17e2c7d4c10bb18466ac121cc
Ahmed Roble <airoble2019@gmail.com>
2022-01-12 19:38:18 -0500

    Change Apollo uri to Heroku instance

--- src/apollo/apolloClient.js
+++ src/apollo/apolloClient.js
@@ -8,9 +8,7 @@ const uploadLink = createUploadLink({
   project should atleast include the backend url for dev environment
   It should only be injected as a process environment variable if this app will be deployed to multiple servers, otherwise it should be an application environment variable
   */
-  uri:
-    `${process.env.REACT_APP_BACKEND_URL}` ||
-    "https://stormy-sands-93557.herokuapp.com/graphql",
+  uri: `https://stormy-sands-93557.herokuapp.com`,
 })
 
 const authLink = setContext((_, { headers }) => {
